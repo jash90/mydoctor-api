ef236a047d037260f214cb47380fbf759f960b4e
Bartłomiej Zimny <bartlomiejzimny@outlook.com>
2019-06-21 22:49:58 +0200

    getAll methods

--- routes/pantient/getAll.js
+++ routes/pantient/getAll.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
 const { Pantient } = require("../../models");
+router.get('/:id', function (req, res, next) {
+    Pantient.findAll({
+        limit: 100,
+        offset: req.params.id * 100
+    }).then(items => {
+        res.json(items);
+    })
+});
 router.get('/', function (req, res, next) {
     Pantient.findAll({
+        limit: 100
     }).then(items => {
         res.json(items);
     })
--- routes/visit/getAll.js
+++ routes/visit/getAll.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
 const { Visit } = require("../../models");
+router.get('/:id', function (req, res, next) {
+    Visit.findAll({
+        limit: 100,
+        offset: req.params.id * 100
+    }).then(items => {
+        res.json(items);
+    })
+});
 router.get('/', function (req, res, next) {
     Visit.findAll({
+        limit:100
     }).then(items => {
         res.json(items);
     })
