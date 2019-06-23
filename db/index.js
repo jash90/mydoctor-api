7ab7b386b96a7780eb72bf7cdcb2229a28624da9
Bartłomiej Zimny <bartlomiejzimny@outlook.com>
2019-06-23 13:23:33 +0200

    change pas

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('myDoctor', 'admin', 'posgress', {
+const sequelize = new Sequelize('myDoctor', 'admin', 'postgress', {
     host: 'localhost',
     dialect: 'postgres',
     operatorsAliases: false,
